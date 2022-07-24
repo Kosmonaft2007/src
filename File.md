719dacf38e9502cd6d9f17ee355a14e322e174ae
Vadim <kosmonaft2007@ya.ru>
2022-07-24 14:35:23 +0300

    Initial commit

---
+++ File.md
@@ -0,0 +1,33 @@
+-
+sudo cd /путь файла 
+
+git config --global user.name "Kosmonaft36"
+git config --global user.email kosmonaft2007@ya.ru
+
+ 1) Шаг 1 
+ - git config --list // провенка на наличие внесыннх данные, с конца 5 и 6 строчка 
+ 2) Шаг 2
+ - git remote add origin https://github.com/Kosmonaft2007/GeekBrains1.git 
+ //ссылка с репозитория 
+ // - fatal: remote origin already exists. // фатальный: удаленный источник уже существует.
+ 3) Шаг 3 
+ - git push -u origin master 
+ // Branch 'master' set up to track remote branch 'master' from 'origin'.
+Everything up-to-date // Ветвь "master" настроена для отслеживания удаленной ветви "master" из "origin".
+Все в актуальном состоянии
+-------
+
+1) git status 
+1.1) On banch master // Находится на ветки мастера 
+1.2) No commit yet //  нет комментариев 
+-
+2) git init - создает репазиторий (инициализирует) 
+3) git add 
+4) git comit
+
+5.0) !!! по окончанию чтения  LOG  нажать на клавишу (Q)
+5) git log  // чтение commit
+5.1) git log --oneline
+-
+6) git checkout + № номер комита 2345 
+7) !!!! git checkout master // после просмотра комит и векруться к первоначальной нужно уйти на ветку мастер!!!
--- Lessons2.java
+++ Lessons2.java
@@ -1,19 +1,15 @@
 
-
-
-
 // import java.util.Scanner;
-
 
 class Lessons2_1 {
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//        int [] mas = new int [n];
-        double[] mas = {0.22, 0.4, 0.92, 1.5, 1.99, 4.5};
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int [] mas = new int [n];
+        double[] mas = { 0.22, 0.4, 0.92, 1.5, 1.99, 4.5 };
         int Max = 0;
         int Min = 0;
-        System.out.println ("\n" + "Pезультат:");
+        System.out.println("\n" + "Pезультат:");
         for (int i = 1; i < mas.length; i++) {
             if (mas[i] > mas[Max]) {
                 Max = i;
@@ -21,40 +17,43 @@ class Lessons2_1 {
                 Min = i;
             }
         }
-        System.out.println("Максимальное значение  " +  mas[Max] + "\n" + "Минимальное значение  " + mas[Min]);
+        System.out.println("Максимальное значение  " + mas[Max] + "\n" + "Минимальное значение  " + mas[Min]);
     }
 }
+
 /*
-Задание на «разворот» массива. Нужно перевернуть массив и записать его в обратном порядке.
+ * Задание на «разворот» массива. Нужно перевернуть массив и записать его в
+ * обратном порядке.
  */
 class Lessons2_2 {
     public static void main(String[] args) {
 
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//        int [] mas = new int [n];
-        int [] mas = {2,4,5,7,8};
-        System.out.println ("Заданая последовательность:");
-        for (int i = 0; i < mas.length; i++){
-            System.out.print ( mas[i] + " ");
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int [] mas = new int [n];
+        int[] mas = { 2, 4, 5, 7, 8 };
+        System.out.println("Заданая последовательность:");
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");
         }
-        System.out.println ("\n" + "Pезультат:");
-        for (int i = mas.length - 1; i >= 0 ; i--)
+        System.out.println("\n" + "Pезультат:");
+        for (int i = mas.length - 1; i >= 0; i--)
 
-            System.out.print ( mas[i]+ "\s");
+            System.out.print(mas[i] + "\s");
     }
 }
 
 /*
-Найти сумму элементов массива, лежащих между максимальным и минимальным по значению элементами
+ * Найти сумму элементов массива, лежащих между максимальным и минимальным по
+ * значению элементами
  */
 class Lessons2_3 {
     public static void main(String[] args) {
-        int [] mas = {2, 4, 9, 1, 3, 7};
+        int[] mas = { 2, 4, 9, 1, 3, 7 };
         int Max = 0;
         int Min = 0;
         int sum = 0;
-        System.out.println ("\n" + "Pезультат:");
+        System.out.println("\n" + "Pезультат:");
         for (int i = 1; i < mas.length; i++) {
             if (mas[i] > mas[Max]) {
                 Max = i;
@@ -62,31 +61,31 @@ class Lessons2_3 {
                 Min = i;
             }
         }
-        System.out.println("Максимальное значение  " +  mas[Max] + "\n" + "Минимальное значение  " + mas[Min]);
+        System.out.println("Максимальное значение  " + mas[Max] + "\n" + "Минимальное значение  " + mas[Min]);
 
         for (int i = 0; i < mas.length; i++) {
-             sum += mas[i];
+            sum += mas[i];
         }
         System.out.println("Pезультат Sum:  " + (sum - mas[Max] - mas[Min]));
     }
 }
 
 /*
- *Найти среднее арифметическое среди всех элементов массива.
+ * Найти среднее арифметическое среди всех элементов массива.
  */
 class Lessons2_4 {
     public static void main(String[] args) {
-        int [] mas = {5, 5, 2};
+        int[] mas = { 5, 5, 2 };
         double sum = 0;
         double res = 0;
 
-        for (int i = 1; i < mas.length; i++){
+        for (int i = 1; i < mas.length; i++) {
 
         }
 
         for (int i = 0; i < mas.length; i++) {
             sum += mas[i];
-            res = sum /  mas.length;
+            res = sum / mas.length;
         }
         System.out.println("Pезультат:  " + res);
     }
